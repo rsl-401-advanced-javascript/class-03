52b41a8bef3ccf4a1bdc410cc3dd1aa7fd299cc6
RoryLetteney <rory.letteney@gmail.com>
2019-05-23 16:36:42 -0500

    Unconverted it from a Promise so it actually works with promisify

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -2,23 +2,12 @@
 
 module.exports = exports = {};
 
-// exports.readFile = file => {
-//   if (!file || file.match(/bad/i)) {
-//     throw new Error(`${file} has an error`);
-//   } else {
-//     setTimeout(() => {
-//       return new Buffer(`${file} contents!`);
-//     }, Math.floor(Math.random() * 50));
-//   }
-// };
-
 exports.readFile = (file, cb) => {
-  if(!file || file.match(/bad/i) ) {
+  if (!file || file.match(/bad/i)) {
     cb(`${file} has an error`);
-  }
-  else {
+  } else {
     setTimeout(() => {
-      cb(undefined, new Buffer(`${file} contents!`));
+      cb(null, `${file} contents!`);
     }, Math.floor(Math.random() * 50));
   }
 };
