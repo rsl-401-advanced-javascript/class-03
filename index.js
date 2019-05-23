18e48d9be3b7cfd78bf7a7476c4e937cb58c806f
RoryLetteney <rory.letteney@gmail.com>
2019-05-23 15:25:33 -0500

    Added all method calls to retrieve data from the files.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
-// const fileReaderA = require('./lib/reader-async.js');
+const fileReaderA = require('./lib/reader-async.js');
 const fileReaderP = require('./lib/reader-promise.js');
 const fileReaderC = require('./lib/reader-callback.js');
+const fileReaderPA = require('./lib/reader-promise-all.js');
 
 // Obtain and assert input
 let files = process.argv.slice(2);
@@ -12,9 +13,15 @@ if( ! (files instanceof Array && files.length) ) {
   throw new Error('Invalid Args');
 }
 
-fileReaderP(files)
-  .then(results => console.log('RESULTS FROM PROMISE METHOD', results));
-
 fileReaderC(files, (err, results) => {
   console.log('RESULTS FROM CALLBACK METHOD', results);
 });
+
+fileReaderP(files)
+  .then(results => console.log('RESULTS FROM PROMISE METHOD', results));
+
+fileReaderA(files)
+  .then(results => console.log('RESULTS FROM ASYNC METHOD', results));
+
+fileReaderPA(files)
+  .then(results => console.log('RESULTS FROM PROMISE All METHOD', results));
