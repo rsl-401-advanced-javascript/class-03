dc1aa4cd72d6f1f0ed4f3bc56eb6ffd6b692637b
RoryLetteney <rory.letteney@gmail.com>
2019-05-22 19:49:30 -0500

    Changed readerjs to export readAll instead of the start function. Console logging the data from the files via indexjs.

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const fileReader = require('./lib/reader.js');
 
 // Obtain and assert input
 let files = process.argv.slice(2);
+files = files.map(file => `${__dirname}/files/${file}`);
 
 if( ! (files instanceof Array && files.length) ) {
   throw new Error('Invalid Args');
 }
 
-fileReader(files, (err,data) => {
-  if ( err ) { throw err; }
-  console.log('From Callback:', data);
-});
+fileReader(files)
+  .then(results => console.log(results));
