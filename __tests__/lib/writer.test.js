6e8a46e0c26e98010388e2c3abefce4fa42f2166
RoryLetteney <rory.letteney@gmail.com>
2019-05-24 11:55:45 -0500

    Moved file reading from writer test to writer. File writing to existing files. Test passing.

--- __tests__/lib/writer.test.js
+++ __tests__/lib/writer.test.js
@@ -1,28 +1,15 @@
 'use strict';
 
-const fs = require('fs');
-const util = require('util');
 const writer = require('../../lib/writer.js');
-const fsReadFile = util.promisify(fs.readFile);
 
 describe('Read and Write to a File', () => {
 
   it.only('should return new file data', async () => {
     const file = `${__dirname}/../../files/test.txt`;
-    let prevContents = Math.random().toString();
-    
-    if (fs.access(file, err => console.log('That file doesn\'t exist!', err))) {
-      prevContents = await fsReadFile(file);
-      prevContents = prevContents.toString().trim();
-    }
 
-    await writer(file);
-    let newContents = await fsReadFile(file);
-    newContents = newContents.toString().trim();
+    const contents = await writer(file);
 
-    console.log(`newContents: ${newContents}\nprevContents: ${prevContents}`);
-
-    expect(newContents).not.toBe(prevContents);
+    expect(contents[0]).not.toBe(contents[1]);
 
   });
 
