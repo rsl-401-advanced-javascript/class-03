ef352f0da7f1b0128c2e11632a14b601432b824b
RoryLetteney <rory.letteney@gmail.com>
2019-05-24 12:33:11 -0500

    Added call to writer for testing with CLI

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fileReaderA = require('./lib/reader-async.js');
 const fileReaderP = require('./lib/reader-promise.js');
 const fileReaderC = require('./lib/reader-callback.js');
 const fileReaderPA = require('./lib/reader-promise-all.js');
+const writer = require('./lib/writer.js');
 
 // Obtain and assert input
 let files = process.argv.slice(2);
@@ -13,15 +14,20 @@ if( ! (files instanceof Array && files.length) ) {
   throw new Error('Invalid Args');
 }
 
-fileReaderC(files, (err, results) => {
-  console.log('RESULTS FROM CALLBACK METHOD', results);
-});
+if (files.length >= 3) {
+  fileReaderC(files, (err, results) => {
+    console.log('RESULTS FROM CALLBACK METHOD', results);
+  });
+  
+  fileReaderP(files)
+    .then(results => console.log('RESULTS FROM PROMISE METHOD', results));
+  
+  fileReaderA(files)
+    .then(results => console.log('RESULTS FROM ASYNC METHOD', results));
+  
+  fileReaderPA(files)
+    .then(results => console.log('RESULTS FROM PROMISE All METHOD', results));
+}
 
-fileReaderP(files)
-  .then(results => console.log('RESULTS FROM PROMISE METHOD', results));
-
-fileReaderA(files)
-  .then(results => console.log('RESULTS FROM ASYNC METHOD', results));
-
-fileReaderPA(files)
-  .then(results => console.log('RESULTS FROM PROMISE All METHOD', results));
+writer(files[0])
+  .then(results => console.log(results));
